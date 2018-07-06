3b0bb0b7d68f61ec40fe67ec21d95d09dd220605
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-05 21:30:16 -0700

    Added kernel module for tasklet

---
+++ 04-tasklet/tasklet-01.c
@@ -0,0 +1,34 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+
+/* for tasklets API */
+char tasklet_data[]="We use a string; but it could be pointer to a structure";
+
+/* Tasklet handler, that just print the data */
+void tasklet_work(unsigned long data)
+{
+	printk("%s\n", (char *)data);
+}
+
+DECLARE_TASKLET(my_tasklet, tasklet_work, (unsigned long) tasklet_data);
+
+static int __init my_init(void)
+{
+	/*
+	 * Schedule the handler.
+	 * Tasklet arealso scheduled from interrupt handler
+	 */
+	tasklet_schedule(&my_tasklet);
+
+	return 0;
+}
+
+void my_exit(void)
+{
+	tasklet_kill(&my_tasklet);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+MODULE_LICENSE("GPL");
